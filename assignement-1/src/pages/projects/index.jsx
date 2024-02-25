3b4b48d2ce7fa307e66d86744926b1392c819adc
mousbah allouch <alloushmousbah1999@gmail.com>
2024-02-25 23:29:34 +0200

    project page

--- assignement-1/src/pages/projects/index.jsx
+++ assignement-1/src/pages/projects/index.jsx
@@ -3,8 +3,29 @@ import './styles.css';
 
 export default function Projects() {
   return (
-    <div className="text-white">
-      This text will be white.
+    <div className="container">
+
+      <div className='project-example'>
+
+      <div class="image-container">
+      </div>
+        <div className='project-example-text'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vulputate tristique quam felis. Id phasellus dui orci vulputate consequat nulla proin. Id sit scelerisque neque, proin bibendum diamLorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vulputate tristique quam felis. Id phasellus dui orci vulputate consequat nulla proin. Id sit scelerisque neque, proin bibendum diam.</p>
+        </div> 
+      </div>
+
+      <div className='project-example'>
+      <div className='project-example-text'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vulputate tristique quam felis. Id phasellus dui orci vulputate consequat nulla proin. Id sit scelerisque neque, proin bibendum diamLorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vulputate tristique quam felis. Id phasellus dui orci vulputate consequat nulla proin. Id sit scelerisque neque, proin bibendum diam.</p>
+        </div>
+      <div class="image-container">
+      </div>
+        
+      </div>
+      
+
+     
     </div>
+
   )
 }
